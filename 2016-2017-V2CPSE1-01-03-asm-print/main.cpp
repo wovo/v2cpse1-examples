147f6ea27a3959536215c5a6e979332430123d19
wovo <wouter@voti.nl>
2017-09-13 16:54:50 +0200

    missing project

--- 2016-2017-V2CPSE1-01-03-asm-print/main.cpp
+++ 2016-2017-V2CPSE1-01-03-asm-print/main.cpp
@@ -7,7 +7,7 @@ extern "C" void uart_put_char( char c ){
 
 extern "C" void print_asciz( const char * s );
 
-extern "C" void application( const char * s ){
+extern "C" void application(){
    print_asciz( "Hello world\n" );	
 }
 
