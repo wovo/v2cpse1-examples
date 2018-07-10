e5fcbf1bebd66892e5a38244d493457bf8407ab0
Wouter van Ooijen <wouter@voti.nl>
2018-07-10 12:29:18 +0200

    update

---
+++ 01-01-message/main.cpp
@@ -0,0 +1,15 @@
+#include "hwlib.hpp"
+#include "message.hpp"
+
+int main( void ){	
+    
+   // kill the watchdog
+   WDT->WDT_MR = WDT_MR_WDDIS;
+   
+   namespace target = hwlib::target;   
+    
+      // wait for the PC console to start
+   hwlib::wait_ms( 500 );
+
+   hwlib::cout << splash();
+}
