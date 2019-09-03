7f42c0a085480c31ab85bd548fabf5eb9fde6f2a
Wouter van Ooijen <wouter@voti.nl>
2019-09-03 14:13:06 +0200

    update

---
+++ practicum-print-asm/main.cpp
@@ -0,0 +1,26 @@
+#include "hwlib.hpp"
+
+void uart_put_char( char c ){
+   hwlib::cout << c;
+}
+
+void print_asciz( const char * s ){
+   while( *s != '\0'){
+      uart_put_char( *s );
+      ++s;
+   }
+}
+
+void application(){
+   print_asciz( "Hello world\n" );	
+}
+
+int main( void ){	
+   
+   namespace target = hwlib::target;   
+    
+      // wait for the PC console to start
+   hwlib::wait_ms( 2000 );
+
+   application();
+}
